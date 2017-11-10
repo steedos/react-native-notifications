1a726752e4e6e197ebe07c7b174d4c5eae2e7447
Elias Nahum <nahumhbl@gmail.com>
2017-11-10 10:12:59 -0300

    Fix notification object validation

--- notification.ios.js
+++ notification.ios.js
@@ -24,6 +24,7 @@ export default class IOSNotification {
       this._type = "managed";
       this._thread = notification.aps["thread-id"];
     } else if (
+      notification &&
       notification.aps &&
       notification.aps.alert) {
       // regular notification
